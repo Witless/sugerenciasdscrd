75601805795413a22064705870a174480e5f5fb6
Mario <42234092+ThePhoDit@users.noreply.github.com>
2020-05-20 16:05:21 +0200

    Updated DB behavior (#4)
    * Bot start.
    
    * DB Path Fix - No sense
    
    * Typo
    
    * Remove db json create

---
+++ linux.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Vamos a configurar tu bot."
+echo "Token del Bot:"
+read bot_token
+echo "Prefix del Bot:"
+read bot_prefix
+echo "Canal para Sugerencias:"
+read suggestions_chnl
+echo "{
+  \"token\": \"$bot_token\",
+  \"prefix\": \"$bot_prefix\",
+  \"channel\": \"$suggestions_chnl\"
+}" > config.json
+echo "Archivo de configuracion creado correctamente."
+  
